81fff5477ea0102dc6e1280e7787fe0baab2af61
Alex <lexuar2000@gmail.com>
2018-11-21 23:27:44 +0300

    Fixed bugs

--- parse.py
+++ parse.py
@@ -1,9 +1,13 @@
-import urllib.request
+from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
 
 def get_html(url):
-    response = urllib.request.urlopen(url)
+    req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+    response = urlopen(req)
     return response.read()
+
+def parse(html):
+    soup = BeautifulSoup(html)
 
 def main():
     print(get_html("http://lttlword.ru/tag/rimworld-v1/"))

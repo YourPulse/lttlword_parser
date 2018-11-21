c4c636eb2f658dbe057c1420b72f2964495189f4
Alex <lexuar2000@gmail.com>
2018-11-21 23:15:15 +0300

    imported bs4

--- parse.py
+++ parse.py
@@ -1,1 +1,12 @@
-print('hello, world')
+import urllib.request
+from bs4 import BeautifulSoup
+
+def get_html(url):
+    response = urllib.request.urlopen(url)
+    return response.read()
+
+def main():
+    print(get_html("http://lttlword.ru/tag/rimworld-v1/"))
+
+if __name__ == '__main__':
+    main()
